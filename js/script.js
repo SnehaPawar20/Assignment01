f57c09c874074f5d5e14525ebb9a7168a636861e
sneha pawar <snehan62@gmail.com>
2024-02-21 00:39:08 -0800

    init commit

---
+++ js/script.js
@@ -0,0 +1,154 @@
+//STEP 1 - Convert to Camel Case notation.
+
+//let someMonth ;
+//function theMonth() ;
+//let currentMonth;
+//let summerMonth;
+//let myLibraryFunction;
+
+//STEP 2 -
+// example of numeric literal expression
+//let age = 30;
+//console.log(`My current age is ${age} years old.`);
+
+//example of string literal expression
+//let myName = "Sneha Pawar";
+//console.log(`My full name is ${myName}`);
+
+//example of Boolean literal expression
+//let isValue = true;
+//console.log(`Value of this expression is ${isValue}`);
+
+//example of null literal expression
+//let p = null;
+//console.log(`Value of this expression is ${p}`);
+
+//STEP 3 - two examples of complex / variable expressions.
+//let x = 3 + 2 * 4;
+//alert(x);
+
+//let y = (4 + 6) / 5;
+//alert(y);
+
+//STEP 4 - Declare (but do not assign)
+//let strFirstName;           //Hungarian Notation
+//let lastName;               //Camel Casing
+//let strAddress;             //Hungarian Notation
+//let strCity;                //Hungarian Notation
+//let strState;               //Hungarian Notation
+//let zipCode;                 //Camel Casing
+//let intYourAge;              //Hungarian Notation
+//let strReferralSource;          //Hungarian Notation
+//let blnMayWeContactYou;         //Hungarian Notation
+
+//STEP 5
+//let strFirstName;
+//let strLastName;
+//let strAddress;
+
+//strFirstName = "Sneha";
+//strLastName = "Pawar";
+//strAddress = "California, USA";
+
+//OR
+//let strFirstName = "Sneha";
+//let strLastName = "Pawar";
+//let strAddress = "California, USA";
+
+//OR
+//let strFirstName = "Sneha", strLastName = "Pawar", strAddress = "California, USA";
+
+//STEP 6
+//let currentYear = 2024;
+//console.log("Happy New Year " + currentYear + ".");
+
+//STEP 7
+//let blnMayWeContactYou = true;
+//console.log(
+// "We will contact you only if ' May we contact you ' value is : " +
+//   blnMayWeContactYou +
+//  ""
+//);
+
+//let result = false;
+//console.log(" 3 + 2 = 6  this expressions returns value " + result + ".");
+
+//STEP 8
+//let someString =
+// 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+//console.log(someString);
+
+//STEP 9
+//let a = null;
+//console.log(a);
+
+//let b;
+//console.log(b);
+
+//STEP 10
+//console.log(typeof "Good Day");
+//console.log(typeof 10);
+//console.log(typeof true);
+
+//let address = new Object();
+//console.log(typeof address);
+
+//let x;
+//console.log(typeof x);
+
+//STEP 11
+//let yourName = "Zak Ruvalcaba";
+
+//alert("Hello " + yourName + ", Welcome to the JavaScript class!");
+
+//STEP 12
+//let name = "Sneha";
+//alert("Hello " + name + ", Welcome to the JavaScript class!");
+
+//STEP 13
+//let yourName = "Zak Ruvalcaba";
+//let course = "JavaScript";
+//alert("Hello " + yourName + ", Welcome to the " + course + " class!");
+
+//STEP 14
+//let stringValue = "Hello Zak Ruvalcaba.\nWelcome to the JavaScript class!";
+//alert(stringValue);
+
+//STEP 15
+//let name = prompt("Enter your Name:");
+//alert("Hello " + name + ", Welcome to the JavaScript class!");
+
+//STEP 16
+//let name = prompt("Enter your Name:");
+//let course = prompt("Which class are you taking?");
+//alert("Hello " + name + ", Welcome to the " + course + " class!");
+
+//STEP 17
+//let x = 10;
+//let y = 20;
+//console.log(x + y);
+
+//STEP 18
+//let x = 20;
+//x += 20;
+//console.log(x);
+
+//STEP 19
+//let x = 20;
+//x *= 5;
+//console.log(x);
+
+//STEP 20
+//let x = 20 % 3;
+//x /= 1;
+//console.log(x);
+
+//STEP 21
+//let x = 20;
+//let y = 3;
+//console.log(x == 20 && y == 3);
+
+//STEP 22
+//let x = 10;
+//let y = 50;
+//console.log(x < 5 || y != 50);
